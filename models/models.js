96185c8fac613a58dd3d4abfb5fc567f48329062
Pavel <pal5555@mail.ru>
2025-03-04 08:29:30 +0300

    'seting_cors'

--- models/models.js
+++ models/models.js
@@ -3,30 +3,36 @@ const {DataTypes} = require ('sequelize')
 
 const User = sequelize.define('User',{
     id:{type: DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
-    name:{type: DataTypes.STRING, },
     email:{type: DataTypes.STRING, unique: true},
     password:{type: DataTypes.STRING},
     role:{type: DataTypes.STRING, defaultValue: 'GUEST'},
+
+})
+const UserInfo = sequelize.define('UserInfo',{
+    id:{type:DataTypes.INTEGER,  primaryKey: true, autoIncrement:true},
+    name:{type:DataTypes.STRING,  allowNull: false},
     img: {type: DataTypes.STRING, allowNull: false},
- 
+    userId: {type: DataTypes.INTEGER,references: {model: User ,key: 'id'}}
 
 })
 
-const Post = sequelize.define('Post',{
-    id:{type: DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
-    title:{type: DataTypes.STRING, },
-    description:{type: DataTypes.STRING},
-    img: {type: DataTypes.STRING, allowNull: false},
-    autor:{type:DataTypes.STRING},
-    userId: {type: DataTypes.INTEGER,references: {model: 'Users',key: 'id'}}
-
-})
+const Post = sequelize.define('Post', {
+    id: {type: DataTypes.INTEGER,autoIncrement: true,primaryKey: true},
+    title: {type: DataTypes.STRING,allowNull: false},
+    description: {type: DataTypes.TEXT},
+    userId: {type: DataTypes.INTEGER,references: {model: User,  key: 'id'}}
+  });
 
 User.hasMany(Post, { foreignKey: 'userId' });
 Post.belongsTo(User, { foreignKey: 'userId' });
+
+User.hasOne(UserInfo, { foreignKey: 'userId' });
+UserInfo.belongsTo(User);
+
 
 
 module.exports={
     User,
     Post,
+    UserInfo
 }
