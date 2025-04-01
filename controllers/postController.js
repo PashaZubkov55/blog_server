997f6fb38b81eb86e2931ab9552d712a90ace7ff
Pavel <pal5555@mail.ru>
2025-04-01 13:19:11 +0300

    'inique_file_name'

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,5 @@
+const uuid = require('uuid')
+
 class PostController{
 async getAll(req, res){
     
@@ -7,7 +9,7 @@ async getAll(req, res){
 async create (req, res){
     const {title, description, userId} = req.body
     const {img} = req.files
-    
+     const fileName = uuid.v4() + '.jpg'
 }
 async getPost(){
 
